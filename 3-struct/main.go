6474dc45e94fc142e971e6ff527b590865575e0f
Dmitry Podkopaev <dmitry.podkopaev@ladadigit.io>
2026-05-23 12:37:30 +0300

    decomposition structure

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,37 +1,3 @@
 package main
 
-import "time"
-
-type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-type BinList struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
-}
-
-func NewBin(id string, private bool, name string) Bin {
-	return Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-}
-
-func NewBinList(id string, private bool, name string) BinList {
-	return BinList{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
-	}
-}
-
 func main() {}
